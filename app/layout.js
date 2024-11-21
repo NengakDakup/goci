01ba5da5921c530b0a3708b5a8f001dcd74f2b54
Nengak Dakup <nengakdakup@gmail.com>
2024-11-21 17:13:19 +0100

    Remove preloader

--- app/layout.js
+++ app/layout.js
@@ -32,12 +32,12 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        <div id="ftco-loader" className="show fullscreen">
+        {/* <div id="ftco-loader" className="show fullscreen">
           <svg className="circular" width="48px" height="48px">
             <circle className="path-bg" cx="24" cy="24" r="22" fill="none" strokeWidth="4" stroke="#eeeeee"/>
             <circle className="path" cx="24" cy="24" r="22" fill="none" strokeWidth="4" strokeMiterlimit="10" stroke="#F96D00"/>
           </svg>
-        </div>
+        </div> */}
 
         <Script src="/js/jquery.min.js"></Script>
         <Script src="/js/jquery-migrate-3.0.1.min.js"></Script>
