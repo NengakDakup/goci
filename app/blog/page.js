af8b4d6a66c9bd502e9fc4b900d9f30b0d22d858
Nengak Dakup <nengakdakup@gmail.com>
2025-03-08 10:36:43 +0100

    feat: add blog page

---
+++ app/blog/page.js
@@ -0,0 +1,139 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <>
+      <section className="hero-wrap hero-wrap-2 js-fullheight" style={{ backgroundImage: `url('images/bg_1.jpg')`, height: '844px' }}>
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row no-gutters slider-text js-fullheight align-items-end" style={{ height: '844px' }}>
+            <div className="col-md-9 ftco-animate pb-5 fadeInUp ftco-animated">
+              <p className="breadcrumbs mb-2"><span className="mr-2"><a href="/">Home <i className="fa fa-chevron-right"></i></a></span> <span>Blog <i className="fa fa-chevron-right"></i></span></p>
+              <h1 className="mb-0 bread">Church Blog</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="ftco-section">
+        <div className="container">
+          <div className="row d-flex">
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_1.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_2.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_3.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_4.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_5.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+              <div className="blog-entry align-self-stretch">
+                <a href="blog-single.html" className="block-20" style={{ backgroundImage: "url('images/image_6.jpg')" }}>
+                </a>
+                <div className="text p-4">
+                  <div className="meta mb-2">
+                    <div><a href="#">July 20, 2020</a></div>
+                    <div><a href="#">Admin</a></div>
+                    <div><a href="#" className="meta-chat"><span className="fa fa-comment"></span> 3</a></div>
+                  </div>
+                  <h3 className="heading"><a href="#">Building Holy &amp; Healthy Lives God's</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                  <p><a href="#" className="btn btn-primary">Read more</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col text-center">
+              <div className="block-27">
+                <ul>
+                  <li><a href="#">&lt;</a></li>
+                  <li className="active"><span>1</span></li>
+                  <li><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li><a href="#">5</a></li>
+                  <li><a href="#">&gt;</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default BlogPage
