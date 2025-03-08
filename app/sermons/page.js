24729fdf95acbe69e4291942815d6b92453c197d
Nengak Dakup <nengakdakup@gmail.com>
2025-03-08 10:18:04 +0100

    feat: add sermons page

---
+++ app/sermons/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SermonsPage = () => {
+  return (
+    <>
+      <section className="hero-wrap hero-wrap-2 js-fullheight" style={{ backgroundImage: `url('images/bg_1.jpg')`, height: '844px' }}>
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row no-gutters slider-text js-fullheight align-items-end" style={{ height: '844px' }}>
+            <div className="col-md-9 ftco-animate pb-5 fadeInUp ftco-animated">
+              <p className="breadcrumbs mb-2"><span className="mr-2"><a href="/">Home <i className="fa fa-chevron-right"></i></a></span> <span>Sermons <i className="fa fa-chevron-right"></i></span></p>
+              <h1 className="mb-0 bread">Sermons</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default SermonsPage
